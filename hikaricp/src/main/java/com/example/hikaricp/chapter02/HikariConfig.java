bc3275ee5ba21d3077c09925dccdc8a4eca23e7a
wonsangL <harryandws@gmail.com>
2020-07-10 00:24:16 +0900

    Chapter02 HikariCP 모니터링 예제

---
+++ hikaricp/src/main/java/com/example/hikaricp/chapter02/HikariConfig.java
@@ -0,0 +1,22 @@
+package com.example.hikaricp.chapter02;
+
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariPoolMXBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.management.JMX;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+@Configuration
+public class HikariConfig {
+    @Bean
+    public HikariPoolMXBean poolProxy() throws MalformedObjectNameException {
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        ObjectName objectName = new ObjectName("com.zaxxer.hikari:type=Pool (hikari)");
+        return JMX.newMBeanProxy(mBeanServer, objectName, HikariPoolMXBean.class);
+    }
+}
--- hikaricp/src/main/java/com/example/hikaricp/chapter02/SampleController.java
+++ hikaricp/src/main/java/com/example/hikaricp/chapter02/SampleController.java
@@ -1,6 +1,9 @@
-package com.example.hikaricp.chapter01;
+package com.example.hikaricp.chapter02;
 
 import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariPoolMXBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +13,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-@RestController("chapter01")
-@RequestMapping("/chapter01")
+@Slf4j
+@RestController("chapter02")
+@RequestMapping("/chapter02")
 public class SampleController {
     private HikariDataSource dataSource;
 
-    public SampleController(HikariDataSource dataSource) {
+    private HikariPoolMXBean poolMXBean;
+
+    public SampleController(HikariDataSource dataSource, HikariPoolMXBean poolMXBean) {
         this.dataSource = dataSource;
+        this.poolMXBean = poolMXBean;
     }
 
     @PostMapping("/join")
@@ -27,9 +34,18 @@ public class SampleController {
         PreparedStatement statement = conn.prepareStatement(query);
         statement.setString(1, name);
 
+        logHikariStatus();
+
         statement.execute();
 
         statement.close();
         conn.close();
+
+        logHikariStatus();
+    }
+
+    private void logHikariStatus(){
+        log.info("connections info total: {}, active: {}, idle: {}, await: {}", poolMXBean.getTotalConnections(),
+                poolMXBean.getActiveConnections(), poolMXBean.getIdleConnections(), poolMXBean.getThreadsAwaitingConnection());
     }
 }
